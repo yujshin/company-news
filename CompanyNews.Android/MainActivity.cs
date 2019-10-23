68de3efa00f2516b2f34d689b8a19199ef01fb6c
Phillip LeBlanc <phillip.leblanc@microsoft.com>
2019-10-23 14:31:06 +0900

    Cleaning up the code a bit

--- CompanyNews.Android/MainActivity.cs
+++ CompanyNews.Android/MainActivity.cs
@@ -26,7 +26,7 @@ namespace CompanyNews.Droid
             base.OnCreate(savedInstanceState);
 
             AppCenter.Start("92209e19-94f7-4b7d-a72d-be5695cb5788",
-                   typeof(Analytics), typeof(Crashes), typeof(Push), typeof(Distribute), typeof(Auth), typeof(Data));
+                   typeof(Analytics), typeof(Crashes), typeof(Push), typeof(Distribute));
 
             FFImageLoading.Forms.Platform.CachedImageRenderer.Init(enableFastRenderer: true);
             FFImageLoading.Forms.Platform.CachedImageRenderer.InitImageViewHandler();
--- CompanyNews/MainPage.xaml.cs
+++ CompanyNews/MainPage.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FFImageLoading.Forms;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
 using Xamarin.Forms;
